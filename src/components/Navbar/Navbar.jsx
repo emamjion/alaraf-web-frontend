57a4b2682d7f51580d6392c6b67a4460e1993137
emamjion <emamjion@gmail.com>
2024-05-27 19:26:17 +0600

    initial project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { assets } from '../../assets/assets';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+                <img src={assets.logo} alt="" className="logo" />
+        </div>
+    );
+};
+
+export default Navbar;

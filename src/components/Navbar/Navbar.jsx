bbd44358b8276b978368e4ab54cf152bb45a5f13
emamjion <emamjion@gmail.com>
2024-05-27 21:02:20 +0600

    Navbar and banner section design completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../../assets/assets';
 import './Navbar.css';
 const Navbar = () => {
+    const [menu, setMenu] = useState('home')
+    
     return (
         <div className='navbar'>
                 <img src={assets.logo} alt="" className="logo" />
+                <ul className='navbar-menu'>
+                    <li onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''}>Home</li>
+                    <li onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''}>Menu</li>
+                    <li onClick={() => setMenu('mobile')} className={menu === 'mobile' ? 'active' : ''}>Mobile app</li>
+                    <li onClick={() => setMenu('contact')} className={menu === 'contact' ? 'active' : ''}>Contact Us</li>
+                </ul>
+                <div className='navbar-right'>
+                    <img src={assets.search_icon} alt="" />
+                    <div className='navbar-search-icon'>
+                        <img src={assets.basket_icon} alt="" />
+                        <div className='dot'></div>
+                    </div>
+                    <button>Sign in</button>
+                </div>
         </div>
     );
 };
